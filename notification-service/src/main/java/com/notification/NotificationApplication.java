f192d27707f2e11a6637b578603e01683866f89e
Omar Siddiqui <omar.siddiqui.6627@gmail.com>
2025-07-13 16:24:56 +0500

    Initial commit

---
+++ notification-service/src/main/java/com/notification/NotificationApplication.java
@@ -0,0 +1,14 @@
+package com.task_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+		System.out.println("Notification Service Application Started");
+	}
+
+}
---
+++ user-service/src/main/java/com/task_management_system/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.task_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println("User Service Application Started");
+	}
+
+}
